dbcc10ee93d3d9a22ab7990f9bd52349f0d43b19
free <li5220008@163.com>
2014-05-11 01:50:07 +0800

    view

---
+++ WebRoot/WEB-INF/pages/pifirt/add.jsp
@@ -0,0 +1,78 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 14-5-6
+  Time: 下午4:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="/WEB-INF/pages/header.jsp"%>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div class="container">
+        <form class="form-horizontal" action="${pageContext.request.contextPath}/pifbus/addDo" method="post" role="form">
+            <div class="form-group">
+                <label for="bus_cde" class="col-sm-1">业务码</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="bus_cde" id="bus_cde" placeholder="业务码">
+                </div>
+
+                <label for="app_cde" class="col-sm-1">应用码</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="app_cde" id="app_cde" placeholder="应用码">
+                </div>
+
+                <label for="bus_itm" class="col-sm-1">对应的核算科目号</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="bus_itm" id="bus_itm" placeholder="对应的核算科目号">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="bus_app" class="col-sm-1">对应的应用码</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="bus_app" id="bus_app" placeholder="对应的应用码">
+                </div>
+
+                <label for="bus_nam" class="col-sm-1">业务描述</label>
+                <div class="col-sm-3">
+                    <textarea class="form-control" name="bus_nam" id="bus_nam" placeholder="业务描述" rows="3" ></textarea>
+                </div>
+
+                <label for="upt_dat" class="col-sm-1">最后更新日期</label>
+                <div class="col-sm-3">
+                    <input type="text" onclick="WdatePicker()" class="form-control" name="upt_dat" id="upt_dat" placeholder="最后更新日期">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="upt_tlr" class="col-sm-1">更新柜员</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="upt_tlr" id="upt_tlr" placeholder="更新柜员">
+                </div>
+
+                <label for="bus_kd1" class="col-sm-1">业务大类</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="bus_kd1" id="bus_kd1" placeholder="业务大类">
+                </div>
+
+                <label for="bus_kd2" class="col-sm-1">业务小类</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="bus_kd2" id="bus_kd2" placeholder="业务小类">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-9"></div>
+                <div class="col-sm-3">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
+<%@include file="/WEB-INF/pages/main.jsp"%>
--- src/java/com/gtafe/controller/PifirtController.java
+++ src/java/com/gtafe/controller/PifirtController.java
@@ -1,8 +1,8 @@
 package com.gtafe.controller;
 
 import com.gtafe.dto.Message;
-import com.gtafe.model.Pifbus;
-import com.gtafe.service.IPIFBUSService;
+import com.gtafe.model.Pifirt;
+import com.gtafe.service.IPIFIRTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,66 +13,66 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 /**
- * Desc: 业务码表
+ * Desc: 利率表
  * User: weiguili(li5220008@gmail.com)
  * Date: 14-5-6
  * Time: 上午11:47
  */
 @Controller
-@RequestMapping("/pifbus")
-public class PifbusController extends BaseController {
+@RequestMapping("/pifirt")
+public class PifirtController extends BaseController {
     @Autowired
-    private IPIFBUSService service;
+    private IPIFIRTService service;
 
     @RequestMapping(value = {"/list"},method = RequestMethod.GET)
     public String list(Model model){
-        List<Pifbus> pifbuses = service.selectAll();
-        model.addAttribute("pifbus",pifbuses);
-        return "/pifbus/list";
+        List<Pifirt> pifirtes = service.selectAll();
+        model.addAttribute("pifirt",pifirtes);
+        return "/pifirt/list";
     }
 
     @RequestMapping("/info")
-    public @ResponseBody Pifbus info(String id) {
-        Pifbus pifbus = service.selectByID(id);
-        return pifbus;
+    public @ResponseBody Pifirt info(String id) {
+        Pifirt pifirt = service.selectByID(id);
+        return pifirt;
     }
 
     @RequestMapping(value = {"/add"},method = RequestMethod.GET)
     public String add(Model model) {
-        Pifbus pifbus = new Pifbus();
-        model.addAttribute(pifbus);
-        return "/pifbus/add";
+        Pifirt pifirt = new Pifirt();
+        model.addAttribute(pifirt);
+        return "/pifirt/add";
     }
     @RequestMapping(value = {"/addDo"},method = RequestMethod.POST)
-    public String addDo(Pifbus pifbus,Model model){
+    public String addDo(Pifirt pifirt,Model model){
         Message message = null;
-        service.add(pifbus);
+        service.add(pifirt);
         try {
         } catch (Exception e) {
             e.printStackTrace();
             return errorMessage(model);
         }
-        message = new Message("添加成功!","/pifbus/list","3");
+        message = new Message("添加成功!","/pifirt/list","3");
         model.addAttribute("message",message);
         return "/errorMessage";
     }
 
     @RequestMapping(value = {"/update"},method = RequestMethod.GET)
     public String update(String id, Model model){
-        Pifbus pifbus = service.selectByID(id);
-        model.addAttribute(pifbus);
-        return "/pifbus/update";
+        Pifirt pifirt = service.selectByID(id);
+        model.addAttribute(pifirt);
+        return "/pifirt/update";
     }
 
     @RequestMapping(value = {"/updateDo"},method = RequestMethod.POST)
-    public String updateDoo(Pifbus pifbus,Model model) {
+    public String updateDoo(Pifirt pifirt,Model model) {
         try {
-            service.update(pifbus);
+            service.update(pifirt);
         } catch (Exception e) {
             e.printStackTrace();
             return errorMessage(model);
         }
-        return "redirect:/pifbus/list";
+        return "redirect:/pifirt/list";
     }
 
     @RequestMapping("/delete")
